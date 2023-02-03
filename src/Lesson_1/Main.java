a6772b93959eb715aebad8961cb74b1e2f001a93
Vally <murkhonovbatoha@gmail.com>
2023-02-03 20:01:04 +0800

    Body

---
+++ src/Lesson_1/Main.java
@@ -0,0 +1,16 @@
+package Lesson_1;
+
+public class Main {
+    public static void main(String[] args) {
+        Author author = new Author("Лев", "Гроссман");
+        Book book = new Book(author, "Волшебники", 2009);
+        //soutf - вывод значений без лишних "" и +
+        System.out.printf("%s, %s %s, %d%n", book.getTitle(),
+                book.getAuthor().getFirstName(),
+                book.getAuthor().getLastName(), book.getPublishingBook());
+        //set - setter метод который меняет значение(в данном случае дату издания книги)
+        book.setPublishingBook(2023);
+        System.out.println(book.getTitle() + ", " + author.getFirstName()
+                + " " + author.getLastName() + ", " + book.getPublishingBook());
+    }
+}
